5cb63522da56f046ec551910da7766ee3762403a
ampeo <ampeo1@outlook.com>
2020-11-03 23:54:25 +0300

    Добавьте файлы проекта.

---
+++ IGI/Models/ImageContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IGI.Models
+{
+    public class ImageContext: DbContext
+    {
+        public DbSet<Image> Files { get; set; }
+        public ImageContext(DbContextOptions<ImageContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ IGI/ViewModels/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IGI.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Не указан Username")]
+        public string Username { get; set; }
+
+        public IFormFile Avatar { get; set; }
+
+        [Required(ErrorMessage = "Не указан Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        public string ConfirmPassword { get; set; }
+    }
+}
